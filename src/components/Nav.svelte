99506fa492601ede8c413840746dcce679cb3bc2
Scott Thompson <d_scott_thompson@hotmail.co.uk>
2020-03-17 12:48:54 +0000

    Update Nav.svelte

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,8 +1,6 @@
 <svelte:head>
   
-  <script>
-	  console.log('hello from Nav');
-  </script>
+  <script>console.log('hello from Nav');</script>
   <script src='some-script.js'></script>
   {@html '<script src="some-other-script.js"></script>'}
 </svelte:head>
